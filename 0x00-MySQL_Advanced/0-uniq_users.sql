401f3bf4e06827dba6e569de02d6c68413f9282f
AJIyanu <aderemijoseph@live.com>
2023-01-15 01:09:42 +0100

    users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,8 @@
--- ensuring comment on all lines\n-- to be ddited later
+-- ensuring comment on all lines
+-- to be ddited later
+
+CREATE TABLE IF NOT EXIST users (
+	id INTEGER NOT NULL AUTO INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
