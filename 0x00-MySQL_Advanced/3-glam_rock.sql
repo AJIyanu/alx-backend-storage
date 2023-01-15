39b8fac79dfea229863cfb9f031b8b89391acd13
AJIyanu <aderemijoseph@live.com>
2023-01-15 03:23:04 +0100

    null default

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,1 +1,7 @@
--- ensuring comment on all lines\n-- to be ddited later
+-- ensuring comment on all lines
+-- to be ddited later
+
+SELECT band_name, split - formed as lifespan
+FROM metal_bands
+WHERE style like '%Glam rock%'
+ORDER BY lifespan desc;
