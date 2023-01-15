07edbd9c78a08744aceeb77b6dc0a6c6fa405955
AJIyanu <aderemijoseph@live.com>
2023-01-15 01:56:13 +0100

    default

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- ensuring comment on all lines
 -- to be ddited later
 
-CREATE TABLE IF NOT EXISTS users1 (
+CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
