48e29be31358462e8d49852b08449a9a24a77e1b
AJIyanu <aderemijoseph@live.com>
2023-01-15 01:16:52 +0100

    dxosts$

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- ensuring comment on all lines
 -- to be ddited later
 
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL AUTO INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
