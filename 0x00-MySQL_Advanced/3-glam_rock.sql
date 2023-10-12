d610c00bddf98140ff0e92836e148c585bb99491
AJ Iyanu <aderemijoseph@live.com>
2023-10-12 21:43:47 +0100

    reengineered

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
--- ensuring comment on all lines
--- to be ddited later
+-- list all bands in Glam rock ranked by longetivity
+-- using formed and split and lifespan
 
-SELECT band_name, split - IF(formed = NULL, 2022, formed) as lifespan
+SELECT band_name, IFNULL(split -  formed, 2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%'
 ORDER BY lifespan desc;
