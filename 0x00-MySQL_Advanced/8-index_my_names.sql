bf1bb93d3823650892a8dfddabb8640cacabf5be
AJ Iyanu <aderemijoseph@live.com>
2023-10-15 05:18:55 +0100

    no syntax error

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,6 @@
 -- create index for table names in column names
 -- first letter only is indexed
 
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+-- ALTER TABLE names ADD COLUMN idx_first CHAR(1);
+-- UPDATE names SET idx_first = LEFT(name, 1);
+CREATE INDEX idx_name_first ON names ((SUBSTR(name, 1, 1)));
