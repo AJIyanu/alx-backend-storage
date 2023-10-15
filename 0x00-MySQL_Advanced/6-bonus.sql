51415e5d366e1a00d91e2ee25c5852259dedcee9
AJ Iyanu <aderemijoseph@live.com>
2023-10-15 04:00:23 +0100

    new project

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,1 +1,18 @@
--- ensuring comment on all lines\n-- to be ddited later
+-- Procedure scipt to add new correction for a student
+-- Takes three arguement id project name and score
+
+DELIMITER //
+CREATE PROCEDURE AddBonus (IN users_id INT, IN project_name VARCHAR(255), IN scores INT)
+BEGIN
+   DECLARE project_id INT;
+   SELECT id INTO project_id FROM projects WHERE name = project_name;
+   IF project_id IS NULL THEN
+      INSERT INTO projects (name) VALUES (project_name);
+      SET project_id = LAST_INSERT_ID();
+   END IF;
+
+   INSERT INTO corrections (user_id, project_id, score) VALUES (users_id, project_id, scores);
+END;
+//
+DELIMITER ;
+
