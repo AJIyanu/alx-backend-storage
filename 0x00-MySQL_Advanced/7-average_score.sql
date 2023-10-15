6a6f0124752ce1f9614589f9d614d3028a77e66a
AJ Iyanu <aderemijoseph@live.com>
2023-10-15 04:25:20 +0100

    aferage score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,1 +1,13 @@
--- ensuring comment on all lines\n-- to be ddited later
+-- calculate average score for student
+-- collects the student id and update row
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser (IN users_id INT)
+BEGIN
+   DECLARE avg_score FLOAT;
+   SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = users_id;
+   UPDATE users
+   SET average_score = avg_score WHERE id = users_id;
+END;
+//
+DELIMITER ;
