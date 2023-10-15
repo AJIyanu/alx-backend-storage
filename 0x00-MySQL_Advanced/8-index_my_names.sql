45a2546845b043bcb3b7bf6d2d7d12954cdfa076
AJ Iyanu <aderemijoseph@live.com>
2023-10-15 04:43:37 +0100

    create index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,1 +1,4 @@
--- ensuring comment on all lines\n-- to be ddited later
+-- create index for table names in column names
+-- first letter only is indexed
+
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
