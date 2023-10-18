ac9468c6e159de3d2c8af3430aa9f597e5640a04
AJ Iyanu <aderemijoseph@live.com>
2023-10-18 06:02:03 +0100

    6dot

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""update topics based on name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """reurns nothing just updates topics"""
+    match = {"name": name}
+    update = {"$set": {"topics": topics}}
+    mongo_collection.update_one(match, update)
