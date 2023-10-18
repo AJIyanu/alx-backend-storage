00ac2b1f7e42085382cfb2d4ba9ae3663287283a
AJ Iyanu <aderemijoseph@live.com>
2023-10-18 05:52:38 +0100

    going in

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""insert document into db"""
+
+
+
+def insert_school(mongo_collections, **kwargs) -> str:
+    """returns id of successful insert"""
+    return mongo_collections.insert_one(kwargs).inserted_id 
