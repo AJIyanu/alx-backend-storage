d7addebed86de82b8cd152bc2ea08508748b3498
AJ Iyanu <aderemijoseph@live.com>
2023-10-18 05:41:04 +0100

    pycode

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,10 @@
-// my comment
+#!/usr/bin/env python3
+"""script to lsit all document in a db"""
+
+
+from typing import List
+
+def list_all(mongo_collection) -> List[]:
+    """returns a list of all document and 
+    empty if empty"""
+    return list(mongo_collection.find())
