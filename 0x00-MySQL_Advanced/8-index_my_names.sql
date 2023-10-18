46ea5e09f3190c091f2ed2c471a65b42b13e48ff
AJ Iyanu <aderemijoseph@live.com>
2023-10-18 04:56:36 +0100

    merging

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -3,4 +3,4 @@
 
 -- ALTER TABLE names ADD COLUMN idx_first CHAR(1);
 -- UPDATE names SET idx_first = LEFT(name, 1);
-CREATE INDEX idx_name_first ON names ((SUBSTR(name, 1, 1)));
+CREATE INDEX idx_name_first2 ON names ((LEFT(name, 1)));
